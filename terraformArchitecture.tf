c7f7a8e0eb7c4d36e6357ec7aafd107cd69ee938
viola_yjy <25651224+violayang@users.noreply.github.com>
2019-01-24 10:59:16 -0500

    update terraform

--- terraformArchitecture.tf
+++ terraformArchitecture.tf
@@ -14,7 +14,7 @@ resource "oci_core_virtual_network" "VCN" {
   cidr_block     = "10.0.0.0/16"
   dns_label      = "VCN"
   compartment_id = "${var.compartment_ocid}"
-  display_name   = "VCN"
+  display_name   = "yjyVCN"
 }
 
 //Creating Internet and NAT gateways
