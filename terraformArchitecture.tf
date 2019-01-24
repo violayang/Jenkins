84f4507138197a0bf659750839f8bbbb1e8cea53
viola_yjy <25651224+violayang@users.noreply.github.com>
2019-01-24 10:36:10 -0500

    update infrastructure with updated terraform

--- terraformArchitecture.tf
+++ terraformArchitecture.tf
@@ -10,7 +10,7 @@ provider "oci" {
   region = "us-ashburn-1"
 }
 
-resource "oci_core_virtual_network" "yjyVCN" {
+resource "oci_core_virtual_network" "VCN" {
   cidr_block     = "10.0.0.0/16"
   dns_label      = "VCN"
   compartment_id = "${var.compartment_ocid}"
@@ -22,12 +22,12 @@ resource "oci_core_internet_gateway" "IGW" {
   compartment_id = "${var.compartment_ocid}"
   display_name   = "IGW"
   enabled        = true
-  vcn_id         = "${oci_core_virtual_network.yjyVCN.id}"
+  vcn_id         = "${oci_core_virtual_network.VCN.id}"
 }
 
 resource "oci_core_nat_gateway" "nat_gateway" {
   compartment_id = "${var.compartment_ocid}"
-  vcn_id         = "${oci_core_virtual_network.yjyVCN.id}"
+  vcn_id         = "${oci_core_virtual_network.VCN.id}"
   display_name   = "nat_gateway"
 }
 
@@ -35,7 +35,7 @@ resource "oci_core_nat_gateway" "nat_gateway" {
 resource "oci_core_route_table" "PublicSubnetRT" {
   compartment_id = "${var.compartment_ocid}"
   display_name   = "PublicSubnetRT"
-  vcn_id         = "${oci_core_virtual_network.yjyVCN.id}"
+  vcn_id         = "${oci_core_virtual_network.VCN.id}"
 
   route_rules {
     destination       = "0.0.0.0/0"
@@ -46,7 +46,7 @@ resource "oci_core_route_table" "PublicSubnetRT" {
 resource "oci_core_route_table" "PrivateSubnetRT" {
   compartment_id = "${var.compartment_ocid}"
   display_name   = "PrivateSubnetRT"
-  vcn_id         = "${oci_core_virtual_network.yjyVCN.id}"
+  vcn_id         = "${oci_core_virtual_network.VCN.id}"
 
   route_rules {
     destination       = "0.0.0.0/0"
@@ -58,7 +58,7 @@ resource "oci_core_route_table" "PrivateSubnetRT" {
 resource "oci_core_security_list" "BastionSecurityList" {
   compartment_id = "${var.compartment_ocid}"
   display_name   = "BastionSecurityList"
-  vcn_id         = "${oci_core_virtual_network.yjyVCN.id}"
+  vcn_id         = "${oci_core_virtual_network.VCN.id}"
 
   ingress_security_rules = [
     {
@@ -102,7 +102,7 @@ resource "oci_core_security_list" "BastionSecurityList" {
 resource "oci_core_security_list" "PrivateDB1SecurityList" {
   compartment_id = "${var.compartment_ocid}"
   display_name   = "PrivateDB1SecurityList"
-  vcn_id         = "${oci_core_virtual_network.yjyVCN.id}"
+  vcn_id         = "${oci_core_virtual_network.VCN.id}"
 
   ingress_security_rules = [
     {
@@ -149,7 +149,7 @@ resource "oci_core_security_list" "PrivateDB1SecurityList" {
 resource "oci_core_security_list" "PrivateDB2SecurityList" {
   compartment_id = "${var.compartment_ocid}"
   display_name   = "PrivateDB2SecurityList"
-  vcn_id         = "${oci_core_virtual_network.yjyVCN.id}"
+  vcn_id         = "${oci_core_virtual_network.VCN.id}"
 
   ingress_security_rules = [
     {
@@ -193,15 +193,44 @@ resource "oci_core_security_list" "PrivateDB2SecurityList" {
   ]
 }
 
-//create 3 subnets
-
+//Bastion Subnet
 resource "oci_core_subnet" "BastionSubnet" {
-  availability_domain        = "ToGS:US-ASHBURN-AD-3"
+  availability_domain        = "ToGS:US-ASHBURN-AD-1"
   cidr_block                 = "10.0.0.0/24"
   compartment_id             = "${var.compartment_ocid}"
   display_name               = "BastionSubnet"
   dns_label                  = "bastionDNS"
-  vcn_id                     = "${oci_core_virtual_network.yjyVCN.id}"
+  vcn_id                     = "${oci_core_virtual_network.VCN.id}"
+  prohibit_public_ip_on_vnic = false
+  route_table_id             = "${oci_core_route_table.PublicSubnetRT.id}"
+
+  security_list_ids = [
+    "${oci_core_security_list.BastionSecurityList.id}",
+  ]
+}
+
+resource "oci_core_subnet" "webServerSubnet1" {
+  availability_domain        = "ToGS:US-ASHBURN-AD-1"
+  cidr_block                 = "10.0.3.0/24"
+  compartment_id             = "${var.compartment_ocid}"
+  display_name               = "webServerSubnet1"
+  dns_label                  = "web1DNS"
+  vcn_id                     = "${oci_core_virtual_network.VCN.id}"
+  prohibit_public_ip_on_vnic = false
+  route_table_id             = "${oci_core_route_table.PublicSubnetRT.id}"
+
+  security_list_ids = [
+    "${oci_core_security_list.BastionSecurityList.id}",
+  ]
+}
+
+resource "oci_core_subnet" "webServerSubnet2" {
+  availability_domain        = "ToGS:US-ASHBURN-AD-2"
+  cidr_block                 = "10.0.4.0/24"
+  compartment_id             = "${var.compartment_ocid}"
+  display_name               = "webServerSubnet2"
+  dns_label                  = "web2DNS"
+  vcn_id                     = "${oci_core_virtual_network.VCN.id}"
   prohibit_public_ip_on_vnic = false
   route_table_id             = "${oci_core_route_table.PublicSubnetRT.id}"
 
@@ -211,12 +240,12 @@ resource "oci_core_subnet" "BastionSubnet" {
 }
 
 resource "oci_core_subnet" "db1Subnet" {
-  availability_domain        = "ToGS:US-ASHBURN-AD-3"
+  availability_domain        = "ToGS:US-ASHBURN-AD-1"
   cidr_block                 = "10.0.1.0/24"
   compartment_id             = "${var.compartment_ocid}"
   display_name               = "db1Subnet"
   dns_label                  = "db1DNS"
-  vcn_id                     = "${oci_core_virtual_network.yjyVCN.id}"
+  vcn_id                     = "${oci_core_virtual_network.VCN.id}"
   prohibit_public_ip_on_vnic = true
   route_table_id             = "${oci_core_route_table.PrivateSubnetRT.id}"
 
@@ -231,7 +260,7 @@ resource "oci_core_subnet" "db2Subnet" {
   compartment_id             = "${var.compartment_ocid}"
   display_name               = "db2Subnet"
   dns_label                  = "db2DNS"
-  vcn_id                     = "${oci_core_virtual_network.yjyVCN.id}"
+  vcn_id                     = "${oci_core_virtual_network.VCN.id}"
   prohibit_public_ip_on_vnic = true
   route_table_id             = "${oci_core_route_table.PrivateSubnetRT.id}"
 
@@ -240,15 +269,15 @@ resource "oci_core_subnet" "db2Subnet" {
   ]
 }
 
-//provision two VMs for the databases
+//provision two database systems
 
 resource "oci_core_instance" "dbSystem1" {
-  availability_domain = "ToGS:US-ASHBURN-AD-3"
+  availability_domain = "ToGS:US-ASHBURN-AD-1"
   compartment_id      = "${var.compartment_ocid}"
   shape               = "VM.Standard2.4"
 
   metadata {
-    ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuNyz+7hob57kYFjqr8l03x4kTALjTClgPYVbKqbFGJC18RYWb8Cxykwtrt3UM0M3HaAuGP17WJ5SxwcPSyqnfwvSqcXxfqCZSEh2n+FJdwsQujPC0/6TncK4IFfqfgehT53XteZ5mTNkzGAnBhI0+Mmij5gTJ0WMfEe8jtlqLjtKqCfY1QboU8cbC7HR2SIknrysRfA7lm9rf3LImpnt+s3xXU52Bms+3klV/jPd3mhnVmc7ZVIxihH/cGPaizc0Xi0iBjExpetxFevPc1HbJ7Y2aKByLwqEO36Xr9BqWeStLln/wkLgKp1XGRq/+qBNeu4Tcu8Qlt1giuOtWdyPZ jiayuaya@Violas-MacBook-Pro.local"
+    ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZgJClETAdRFuH4y/uRAgMLuLnn/QL07oZDBEEc9oxCdKCD9nH3GbKykQ+9RP9h29etzOfyDkRF6oB9Dh2ukzhEFcyBIkPVC1Lze1tVjtoXXms3fGpzrYPkq8UxDAwt+k66xuhptR9PSklJspyBEHYAClJN56t4zoRr/ZhJnafZmPQ41QfSWss8JGNiHlqRmlvRLgC/LwRY/q4E1ZE/VfR1RK5eDa6DZOu6UrjTf9fi3BIvoPcLgV7jPW/nFcOiGYSgJ/yq4Dpy8pcfs06DHqR43O4TlWXQ5Ysxr0K1VzbZwwX+Y0o64qQJDthZWbVAoV02oBHKbWzDVI8965HJMyr joboyle@Jamess-MacBook-Pro-2.local"
   }
 
   display_name = "dbSystem1"
@@ -270,7 +299,7 @@ resource "oci_core_instance" "dbSystem2" {
   shape               = "VM.Standard2.4"
 
   metadata {
-    ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuNyz+7hob57kYFjqr8l03x4kTALjTClgPYVbKqbFGJC18RYWb8Cxykwtrt3UM0M3HaAuGP17WJ5SxwcPSyqnfwvSqcXxfqCZSEh2n+FJdwsQujPC0/6TncK4IFfqfgehT53XteZ5mTNkzGAnBhI0+Mmij5gTJ0WMfEe8jtlqLjtKqCfY1QboU8cbC7HR2SIknrysRfA7lm9rf3LImpnt+s3xXU52Bms+3klV/jPd3mhnVmc7ZVIxihH/cGPaizc0Xi0iBjExpetxFevPc1HbJ7Y2aKByLwqEO36Xr9BqWeStLln/wkLgKp1XGRq/+qBNeu4Tcu8Qlt1giuOtWdyPZ jiayuaya@Violas-MacBook-Pro.local"
+    ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZgJClETAdRFuH4y/uRAgMLuLnn/QL07oZDBEEc9oxCdKCD9nH3GbKykQ+9RP9h29etzOfyDkRF6oB9Dh2ukzhEFcyBIkPVC1Lze1tVjtoXXms3fGpzrYPkq8UxDAwt+k66xuhptR9PSklJspyBEHYAClJN56t4zoRr/ZhJnafZmPQ41QfSWss8JGNiHlqRmlvRLgC/LwRY/q4E1ZE/VfR1RK5eDa6DZOu6UrjTf9fi3BIvoPcLgV7jPW/nFcOiGYSgJ/yq4Dpy8pcfs06DHqR43O4TlWXQ5Ysxr0K1VzbZwwX+Y0o64qQJDthZWbVAoV02oBHKbWzDVI8965HJMyr joboyle@Jamess-MacBook-Pro-2.local"
   }
 
   display_name = "dbSystem2"
@@ -289,7 +318,7 @@ resource "oci_core_instance" "dbSystem2" {
 //provision bastion instance
 
 resource "oci_core_instance" "bastionInstance" {
-  availability_domain = "ToGS:US-ASHBURN-AD-3"
+  availability_domain = "ToGS:US-ASHBURN-AD-1"
   compartment_id      = "${var.compartment_ocid}"
 
   source_details {
@@ -300,13 +329,59 @@ resource "oci_core_instance" "bastionInstance" {
   shape = "VM.Standard2.4"
 
   metadata {
-    ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuNyz+7hob57kYFjqr8l03x4kTALjTClgPYVbKqbFGJC18RYWb8Cxykwtrt3UM0M3HaAuGP17WJ5SxwcPSyqnfwvSqcXxfqCZSEh2n+FJdwsQujPC0/6TncK4IFfqfgehT53XteZ5mTNkzGAnBhI0+Mmij5gTJ0WMfEe8jtlqLjtKqCfY1QboU8cbC7HR2SIknrysRfA7lm9rf3LImpnt+s3xXU52Bms+3klV/jPd3mhnVmc7ZVIxihH/cGPaizc0Xi0iBjExpetxFevPc1HbJ7Y2aKByLwqEO36Xr9BqWeStLln/wkLgKp1XGRq/+qBNeu4Tcu8Qlt1giuOtWdyPZ jiayuaya@Violas-MacBook-Pro.local"
+    ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZgJClETAdRFuH4y/uRAgMLuLnn/QL07oZDBEEc9oxCdKCD9nH3GbKykQ+9RP9h29etzOfyDkRF6oB9Dh2ukzhEFcyBIkPVC1Lze1tVjtoXXms3fGpzrYPkq8UxDAwt+k66xuhptR9PSklJspyBEHYAClJN56t4zoRr/ZhJnafZmPQ41QfSWss8JGNiHlqRmlvRLgC/LwRY/q4E1ZE/VfR1RK5eDa6DZOu6UrjTf9fi3BIvoPcLgV7jPW/nFcOiGYSgJ/yq4Dpy8pcfs06DHqR43O4TlWXQ5Ysxr0K1VzbZwwX+Y0o64qQJDthZWbVAoV02oBHKbWzDVI8965HJMyr joboyle@Jamess-MacBook-Pro-2.local"
   }
 
   display_name = "bastionInstance"
 
   create_vnic_details {
     subnet_id        = "${oci_core_subnet.BastionSubnet.id}"
+    assign_public_ip = true
+  }
+}
+
+resource "oci_core_instance" "webServer1" {
+  availability_domain = "ToGS:US-ASHBURN-AD-1"
+  compartment_id      = "${var.compartment_ocid}"
+
+  source_details {
+    source_id   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda"
+    source_type = "image"
+  }
+
+  shape = "VM.Standard2.4"
+
+  metadata {
+    ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZgJClETAdRFuH4y/uRAgMLuLnn/QL07oZDBEEc9oxCdKCD9nH3GbKykQ+9RP9h29etzOfyDkRF6oB9Dh2ukzhEFcyBIkPVC1Lze1tVjtoXXms3fGpzrYPkq8UxDAwt+k66xuhptR9PSklJspyBEHYAClJN56t4zoRr/ZhJnafZmPQ41QfSWss8JGNiHlqRmlvRLgC/LwRY/q4E1ZE/VfR1RK5eDa6DZOu6UrjTf9fi3BIvoPcLgV7jPW/nFcOiGYSgJ/yq4Dpy8pcfs06DHqR43O4TlWXQ5Ysxr0K1VzbZwwX+Y0o64qQJDthZWbVAoV02oBHKbWzDVI8965HJMyr joboyle@Jamess-MacBook-Pro-2.local"
+  }
+
+  display_name = "webServer1"
+
+  create_vnic_details {
+    subnet_id        = "${oci_core_subnet.webServerSubnet1.id}"
+    assign_public_ip = true
+  }
+}
+
+resource "oci_core_instance" "webServer2" {
+  availability_domain = "ToGS:US-ASHBURN-AD-2"
+  compartment_id      = "${var.compartment_ocid}"
+
+  source_details {
+    source_id   = "ocid1.image.oc1.iad.aaaaaaaageeenzyuxgia726xur4ztaoxbxyjlxogdhreu3ngfj2gji3bayda"
+    source_type = "image"
+  }
+
+  shape = "VM.Standard2.4"
+
+  metadata {
+    ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZgJClETAdRFuH4y/uRAgMLuLnn/QL07oZDBEEc9oxCdKCD9nH3GbKykQ+9RP9h29etzOfyDkRF6oB9Dh2ukzhEFcyBIkPVC1Lze1tVjtoXXms3fGpzrYPkq8UxDAwt+k66xuhptR9PSklJspyBEHYAClJN56t4zoRr/ZhJnafZmPQ41QfSWss8JGNiHlqRmlvRLgC/LwRY/q4E1ZE/VfR1RK5eDa6DZOu6UrjTf9fi3BIvoPcLgV7jPW/nFcOiGYSgJ/yq4Dpy8pcfs06DHqR43O4TlWXQ5Ysxr0K1VzbZwwX+Y0o64qQJDthZWbVAoV02oBHKbWzDVI8965HJMyr joboyle@Jamess-MacBook-Pro-2.local"
+  }
+
+  display_name = "webServer2"
+
+  create_vnic_details {
+    subnet_id        = "${oci_core_subnet.webServerSubnet2.id}"
     assign_public_ip = true
   }
 }
