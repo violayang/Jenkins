ba90efee8573d78283dc9bd768a269daec109af7
viola_yjy <25651224+violayang@users.noreply.github.com>
2019-03-06 15:46:44 -0500

    update - comment

--- terraformArchitecture.tf
+++ terraformArchitecture.tf
@@ -17,7 +17,7 @@ provider "oci" {
 }
 
 # provision bastion instance
-
+/*
 resource "oci_core_instance" "bastionInstance" {
   availability_domain = "XqVg:US-ASHBURN-AD-1"
   compartment_id      = "${var.compartment_ocid}"
@@ -42,3 +42,4 @@ resource "oci_core_instance" "bastionInstance" {
 }
 
 
+*/
